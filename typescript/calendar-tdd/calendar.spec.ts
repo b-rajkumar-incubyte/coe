5430848148e8eebeba805740131d269616927b32
Rajkumar Buddha <rajkumar@incubyte.co>
2026-06-07 05:16:35 +0000

    Refactor isLeapYear function

--- typescript/calendar-tdd/calendar.spec.ts
+++ typescript/calendar-tdd/calendar.spec.ts
@@ -12,4 +12,8 @@ describe('isLeapYear', () => {
     it('should be true for year which is divisible by 4 but not by 100', () => {
         expect(isLeapYear(2004)).toBe(true);
     })
+
+    it('should be false for a year which is not divisible by 4', () => {
+        expect(isLeapYear(2017)).toBe(false);
+    })
 });
--- typescript/calendar-tdd/calendar.ts
+++ typescript/calendar-tdd/calendar.ts
@@ -1,4 +1,10 @@
 export default function isLeapYear(year: number): boolean {
-    if(year % 4 === 0 && year % 100 !== 0) return true;  
-    return year % 400 === 0;
+    const isDivisibleBy4 = year % 4 === 0;
+    const isDivisibleBy100 = year % 100 === 0;
+    const isDivisibleBy400 = year % 400 === 0;
+
+    if(isDivisibleBy400) return true;
+    if(isDivisibleBy4 && !isDivisibleBy100) return true;
+
+    return false;
 }
