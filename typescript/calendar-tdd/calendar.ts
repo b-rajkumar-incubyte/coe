1ca2d9601a5edd3963488be1af6172b1f08be75f
Rajkumar Buddha <rajkumar@incubyte.co>
2026-06-07 05:22:26 +0000

    Fix the failing test for edge case when year is 0

--- typescript/calendar-tdd/calendar.ts
+++ typescript/calendar-tdd/calendar.ts
@@ -1,4 +1,5 @@
 export default function isLeapYear(year: number): boolean {
+    if (year === 0) throw new Error("Year must be a non-zero integer");
     const isDivisibleBy4 = year % 4 === 0;
     const isDivisibleBy100 = year % 100 === 0;
     const isDivisibleBy400 = year % 400 === 0;
