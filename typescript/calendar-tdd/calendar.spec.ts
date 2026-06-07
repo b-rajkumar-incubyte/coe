adfe15fa1fdf24569c72b6059f3d54031dcf4a04
Rajkumar Buddha <rajkumar@incubyte.co>
2026-06-07 05:00:01 +0000

    Add test case for non leap year

--- typescript/calendar-tdd/calendar.spec.ts
+++ typescript/calendar-tdd/calendar.spec.ts
@@ -2,6 +2,10 @@ import isLeapYear from "./calendar";
 
 describe('isLeapYear', () => {
     it('should be true for a year divisible by 400', () => {
-        expect(isLeapYear(400)).toBe(true);
+        expect(isLeapYear(1600)).toBe(true);
+    })
+
+    it('should be false for a year which is divisible by 100 by not by 400', () => {
+        expect(isLeapYear(1800)).toBe(false);
     })
 });
