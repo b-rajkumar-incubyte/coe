7095c6fd9a214202a237e4f23354377ee087d97f
Rajkumar Buddha <rajkumar@incubyte.co>
2026-06-08 08:23:33 +0000

    Add post end point to create a task

--- task-management/src/task/task.controller.ts
+++ task-management/src/task/task.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { TaskService } from "./task.service";
 import type { Task } from "./types";
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 
 @Controller("/task")
@@ -11,5 +12,10 @@ export class TaskController {
     @Get()
     getAll(): Task[] {
         return this.taskService.getAll();
+    }
+
+    @Post()
+    createTask(@Body() taskDetails: CreateTaskDto): number {
+        return this.taskService.createTask(taskDetails);
     }
 }
--- task-management/src/task/task.service.ts
+++ task-management/src/task/task.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import type { Task } from "./types";
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 @Injectable()
 export class TaskService {
+    private previousId: number = 0;
     private tasks: Map<number, Task> = new Map();
 
     getAll(): Task[] {
         return [...this.tasks.values()].map((task) => ({...task}));
+    }
+
+    createTask(taskDetails: CreateTaskDto): number {
+        const id = this.previousId + 1;
+        this.previousId = id;
+        const task: Task = {
+            id,
+            title: taskDetails.title,
+            description: taskDetails.description,
+            status: "Not started"
+        }
+
+        this.tasks.set(id, task);
+
+        return id;
     }
 }
